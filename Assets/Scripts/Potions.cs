57dbba5563312beb141cdb496fe73222e6bf2a31
Hopscotch1337 <hopscotch@posteo.de>
2025-06-09 14:02:33 +0200

    Preperations for Potions and Consumables
    - Created and implemented potion sprites
    - Created potion Scriptable Object
    - Created Classes potions and made a Prefab

---
+++ Assets/Scripts/Potions.cs
@@ -0,0 +1,75 @@
+// Potions.cs
+using System.Collections;
+using UnityEngine;
+
+public class Potions : MonoBehaviour
+{
+    [Header("Referenzen")]
+    public PlayerHealth playerHealth;  // Dein Skript, das Health/Speed/Damage/Stamina verwaltet
+
+    /// <summary>
+    /// Wird aufgerufen, wenn der Spieler einen Trank benutzt.
+    /// </summary>
+    public void UsePotion(PotionInfo potion)
+    {
+        foreach (var effect in potion.effects)
+        {
+            switch (effect.effectType)
+            {
+                case PotionEffectType.Heal:
+                    ApplyHeal((int)effect.magnitude);
+                    break;
+                case PotionEffectType.MoveSpeed:
+                case PotionEffectType.Damage:
+                case PotionEffectType.Stamina:
+                    StartCoroutine(ApplyBuff(effect));
+                    break;
+            }
+        }
+    }
+
+    private void ApplyHeal(int amount)
+    {
+        playerStats.CurrentHealth += amount;
+        playerStats.CurrentHealth = Mathf.Min(playerStats.CurrentHealth, playerStats.MaxHealth);
+        Debug.Log($"Healed for {amount} HP.");
+    }
+
+    private IEnumerator ApplyBuff(PotionEffect effect)
+    {
+        // Setze Buff
+        switch (effect.effectType)
+        {
+            case PotionEffectType.MoveSpeed:
+                playerStats.MoveSpeed += effect.magnitude;
+                Debug.Log($"Speed +{effect.magnitude} for {effect.duration}s");
+                break;
+            case PotionEffectType.DamageIncrease:
+                playerStats.DamageMultiplier += effect.magnitude;
+                Debug.Log($"Damage +{effect.magnitude} for {effect.duration}s");
+                break;
+            case PotionEffectType.Stamina:
+                playerStats.StaminaRegen += effect.magnitude;
+                Debug.Log($"Stamina Regen +{effect.magnitude} for {effect.duration}s");
+                break;
+        }
+
+        // Warte Buff-Dauer
+        yield return new WaitForSeconds(effect.duration);
+
+        // Buff zurücksetzen
+        switch (effect.effectType)
+        {
+            case PotionEffectType.MoveSpeed:
+                playerStats.MoveSpeed -= effect.magnitude;
+                break;
+            case PotionEffectType.Damage:
+                playerStats.DamageMultiplier -= effect.magnitude;
+                break;
+            case PotionEffectType.Stamina:
+                playerStats.StaminaRegen -= effect.magnitude;
+                break;
+        }
+        Debug.Log($"{effect.effectType} buff ended.");
+    }
+}
--- Assets/Scripts/Scriptable Objects/Script/PotionInfo.cs
+++ Assets/Scripts/Scriptable Objects/Script/PotionInfo.cs
@@ -1,9 +1,28 @@
+// PotionInfo.cs
 using UnityEngine;
+using System.Collections.Generic;
+
+public enum PotionEffectType
+{
+    Heal,
+    MoveSpeed,
+    DamageIncrease,
+    Stamina
+}
+
+[System.Serializable]
+public class PotionEffect
+{
+    public PotionEffectType effectType;
+    public float magnitude;     // z.B. Heal-Amount oder Prozent-Wert für Buffs
+    public float duration;      // Dauer in Sekunden (für Instant-Effekte: 0)
+}
 
 [CreateAssetMenu(menuName = "Inventory/New Potion")]
 public class PotionInfo : ItemInfo
 {
-    public int healAmount;
+    [Tooltip("Liste aller Effekte dieses Tranks")]
+    public List<PotionEffect> effects = new List<PotionEffect>();
 
     private void OnValidate()
     {
