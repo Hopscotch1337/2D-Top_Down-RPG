d5584932496bd20a7320ca567d2f68053e3085e7
Hopscotch1337 <hopscotch@posteo.de>
2025-06-13 22:00:18 +0200

    Bugfix scene change
    after changing scene, the buyfeature did not work. Because the itemInInventory list was in inspector visible, it created a second list what could not be seen.
    
    now shop feature is presistent after scene change

--- Assets/Scripts/Inventory/InventoryItem.cs
+++ Assets/Scripts/Inventory/InventoryItem.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 [System.Serializable]
 public class InventoryItem
 {
-    public ItemInfo itemInfo;  // Referenz auf dein ScriptableObject (WeaponInfo, PotionInfo…)
+    public ItemInfo itemInfo;  // Referenz auf ScriptableObject (WeaponInfo, PotionInfo…)
     public int quantity;       // Stapel­größe oder Anzahl
 
     public InventoryItem(ItemInfo info, int qty = 1)
